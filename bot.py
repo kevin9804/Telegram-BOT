d0ec8593a32e773ccf319058e482cd15fbfb9110
LEYAO LI <lli225@uottawa.ca>
2021-05-04 11:50:50 -0400

    reply picture test

--- bot.py
+++ bot.py
@@ -40,9 +40,7 @@ def send_help(message):
 @bot.message_handler(commands=['fido_wireless']) # fido wireless message handler
 def send_fido(message):
     photo = open('info/fido/wireless/ON_rate.jpg','rb')
-    bot.send_photo(message.chat.id, photo)
-    bot.send_photo(message.chat.id, "FILEID")
-    bot.send_message(message.chat.id, 'Here is the price plan for Ontario Numbers')
+    bot.send_photo(message.chat.id, photo, caption='Here is the price plan for Ontario Numbers')
 
 @bot.message_handler(commands=['fido_home_internet']) # fido wireless message handler
 def send_fido(message):
